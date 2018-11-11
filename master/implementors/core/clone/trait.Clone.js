0e683e23d6ba481d5368eca8155bfad49da05b8f
Rafał Michalski <royaltm75@gmail.com>
2018-11-11 12:13:59 +0100

    ghp-upload script

---
+++ master/implementors/core/clone/trait.Clone.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["plasma"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"plasma/struct.PhaseAmpCfg.html\" title=\"struct plasma::PhaseAmpCfg\">PhaseAmpCfg</a>",synthetic:false,types:["plasma::phase_amp::PhaseAmpCfg"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"plasma/struct.PhaseAmp.html\" title=\"struct plasma::PhaseAmp\">PhaseAmp</a>",synthetic:false,types:["plasma::phase_amp::PhaseAmp"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"plasma/struct.Plasma.html\" title=\"struct plasma::Plasma\">Plasma</a>",synthetic:false,types:["plasma::plasma::Plasma"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ master/implementors/core/default/trait.Default.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["plasma"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"plasma/struct.PhaseAmp.html\" title=\"struct plasma::PhaseAmp\">PhaseAmp</a>",synthetic:false,types:["plasma::phase_amp::PhaseAmp"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"plasma/struct.PhaseAmpCfg.html\" title=\"struct plasma::PhaseAmpCfg\">PhaseAmpCfg</a>",synthetic:false,types:["plasma::phase_amp::PhaseAmpCfg"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()

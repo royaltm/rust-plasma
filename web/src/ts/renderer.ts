068736f49719e0623cfa19bce8819afec8d83822
Rafał Michalski <royaltm75@gmail.com>
2018-12-03 10:32:27 +0100

    web: reformat typescript files

--- web/src/ts/renderer.ts
+++ web/src/ts/renderer.ts
@@ -13,59 +13,59 @@ import { BitmapGenerator } from './generator';
  *  methods bound to this instance.
  */
 export abstract class BaseRenderer {
-   /** Animation state. */
-   isAnimating: boolean;
-   /** The attached bitmap generator. */
-   generator: BitmapGenerator;
-   protected readyHandler: () => void;
-   protected bitmapHandler: (event: CustomEvent) => void;
-   /**
-    * Creates a new canvas renderer instance.
-    */
-   constructor() {
-      this.isAnimating = false;
-      this.generator = null;
-   }
+    /** Animation state. */
+    isAnimating: boolean;
+    /** The attached bitmap generator. */
+    generator: BitmapGenerator;
+    protected readyHandler: () => void;
+    protected bitmapHandler: (event: CustomEvent) => void;
+    /**
+     * Creates a new canvas renderer instance.
+     */
+    constructor() {
+        this.isAnimating = false;
+        this.generator = null;
+    }
 
-   /**
-    * Attaches a bitmap generator instance to this renderer.
-    */
-   attach(generator: BitmapGenerator): void {
-      this.animate(false);
-      this.detach();
-      this.generator = generator;
-      generator.addEventListener("ready", this.readyHandler, false);
-      generator.addEventListener("bitmap", this.bitmapHandler, false);
-   }
-
-   /**
-    * Detaches a bitmap generator instance from this renderer.
-    */
-   detach(): BitmapGenerator {
-      var generator = this.generator;
-      if (generator) {
-         this.animate(false);
-         generator.removeEventListener("ready", this.readyHandler, false);
-         generator.removeEventListener("bitmap", this.bitmapHandler, false);
-      }
-      this.generator = null;
-      return generator;
-   }
-
-   /**
-    * Controls animation state.
-    */
-   animate(enable: boolean): void {
-      if (!enable === !this.isAnimating) return;
-      if (enable) {
-         this.isAnimating = true;
-         let generator = this.generator;
-         if (generator && generator.isReady) {
-            generator.render();
-         }
-      }
-      else {
-         this.isAnimating = false;
-      }
-   }
+    /**
+     * Attaches a bitmap generator instance to this renderer.
+     */
+    attach(generator: BitmapGenerator): void {
+        this.animate(false);
+        this.detach();
+        this.generator = generator;
+        generator.addEventListener("ready", this.readyHandler, false);
+        generator.addEventListener("bitmap", this.bitmapHandler, false);
+    }
+ 
+    /**
+     * Detaches a bitmap generator instance from this renderer.
+     */
+    detach(): BitmapGenerator {
+        var generator = this.generator;
+        if (generator) {
+            this.animate(false);
+            generator.removeEventListener("ready", this.readyHandler, false);
+            generator.removeEventListener("bitmap", this.bitmapHandler, false);
+        }
+        this.generator = null;
+        return generator;
+    }
+ 
+    /**
+     * Controls animation state.
+     */
+    animate(enable: boolean): void {
+        if (!enable === !this.isAnimating) return;
+        if (enable) {
+            this.isAnimating = true;
+            let generator = this.generator;
+            if (generator && generator.isReady) {
+                 generator.render();
+            }
+        }
+        else {
+            this.isAnimating = false;
+        }
+    }
 }
--- web/src/ts/three.ts
+++ web/src/ts/three.ts
@@ -1,18 +1,18 @@
 /**
  * @module plasma
  */
-/**/
+ /**/
 import { BitmapDetail } from './generator';
 import { BaseRenderer } from './renderer';
 
 // missing from typescript dom declarations
 declare class OffscreenCanvas {
-   height: number;
-   width: number;
-   constructor(width: number, height: number);
-   getContext(contextType: "2d"|"bitmaprenderer", contextAttributes?: {alpha: boolean}): CanvasRenderingContext2D|ImageBitmapRenderingContext;
-   toBlob(type: string, encoderOptions?: number): Promise<Blob>;
-   transferToImageBitmap(): ImageBitmap;
+    height: number;
+    width: number;
+    constructor(width: number, height: number);
+    getContext(contextType: "2d"|"bitmaprenderer", contextAttributes?: {alpha: boolean}): CanvasRenderingContext2D|ImageBitmapRenderingContext;
+    toBlob(type: string, encoderOptions?: number): Promise<Blob>;
+    transferToImageBitmap(): ImageBitmap;
 }
 
 /**
@@ -21,64 +21,60 @@ declare class OffscreenCanvas {
  * Attach an instance of a [[BitmapGenerator]] with a [[TextureRenderer.attach]] method.
  */
 export class TextureRenderer extends BaseRenderer {
-   /** The target Texture. */
-   target: THREE.Texture;
-   protected bitmap: ImageBitmap;
-   protected canvas: OffscreenCanvas;
-   protected ctx: CanvasRenderingContext2D;
+    /** The target Texture. */
+    target: THREE.Texture;
+    protected bitmap: ImageBitmap;
+    protected canvas: OffscreenCanvas;
+    protected ctx: CanvasRenderingContext2D;
    /**
     * Creates a new Texture renderer instance.
     */
-   constructor(target: THREE.Texture) {
-      super();
-      this.target = target;
-      this.bitmap = null;
-      this.canvas = null;
-      this.ctx = null;
-
-      this.readyHandler = () => {
-         requestAnimationFrame(_ts => {
-            var { bitmap, canvas, target: texture } = this
-              , image = texture.image
-
-            if (!this.isAnimating) {
-               if (bitmap !== null) {
-                  bitmap.close();
-                  this.bitmap = null;
-               }
-               return;
+    constructor(target: THREE.Texture) {
+        super();
+        this.target = target;
+        this.bitmap = null;
+        this.canvas = null;
+        this.ctx = null;
+        this.readyHandler = () => {
+            requestAnimationFrame(_ts => {
+                var { bitmap, canvas, target: texture } = this
+                  , image = texture.image
+                if (!this.isAnimating) {
+                    if (bitmap !== null) {
+                        bitmap.close();
+                        this.bitmap = null;
+                    }
+                    return;
+                }
+                if (bitmap !== null || canvas !== null) {
+                    if (bitmap !== null) {
+                        this.bitmap = null;
+                        texture.image = bitmap;
+                    }
+                    else {
+                        texture.image = canvas.transferToImageBitmap();
+                    }
+                    texture.needsUpdate = true;
+                    if (image instanceof ImageBitmap) image.close();
+                }
+                var generator = this.generator;
+                if (generator) generator.render();
+            });
+        };
+        this.bitmapHandler = (ev) => {
+            var { x, y, width, height, bitmap }: BitmapDetail = ev.detail;
+            if (width === bitmap.width && height === bitmap.height) {
+                this.bitmap = bitmap;
             }
-            if (bitmap !== null || canvas !== null) {
-               if (bitmap !== null) {
-                  this.bitmap = null;
-                  texture.image = bitmap;
-               }
-               else {
-                  texture.image = canvas.transferToImageBitmap();
-               }
-               texture.needsUpdate = true;
-               if (image instanceof ImageBitmap) image.close();
+            else {
+                let { canvas, ctx } = this;
+                if (canvas == null || canvas.width !== width || canvas.height !== height) {
+                    canvas = this.canvas = new OffscreenCanvas(width, height);
+                    ctx = this.ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+                }
+                ctx.drawImage(bitmap, x, y);
+                bitmap.close();
             }
-            var generator = this.generator;
-            if (generator) generator.render();
-         });
-      };
-
-      this.bitmapHandler = (ev) => {
-         var { x, y, width, height, bitmap }: BitmapDetail = ev.detail;
-
-         if (width === bitmap.width && height === bitmap.height) {
-            this.bitmap = bitmap;
-         }
-         else {
-            var { canvas, ctx } = this;
-            if (canvas == null || canvas.width !== width || canvas.height !== height) {
-               canvas = this.canvas = new OffscreenCanvas(width, height);
-               ctx = this.ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
-            }
-            ctx.drawImage(bitmap, x, y);
-            bitmap.close();
-         }
-      };
-   }
+        };
+    }
 }

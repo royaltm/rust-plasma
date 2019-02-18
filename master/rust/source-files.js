ff085c91e293b21ffa1bbd1b3c92b9cf049152ea
Rafał Michalski <royaltm75@gmail.com>
2019-02-18 11:38:53 +0100

    update

---
+++ master/rust/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["plasma"] = {"name":"","dirs":[{"name":"mixers","dirs":[],"files":["mod.rs","plasma_default.rs","plasma_experimental.rs","plasma_fire.rs"]}],"files":["color.rs","lib.rs","mixer.rs","phase_amp.rs","pixel_buffer.rs","plasma.rs","simd_polyfill.rs"]};
+createSourceSidebar();

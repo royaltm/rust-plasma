7f463ddd4977966f5e58b681a83aad0655eca51e
Rafal Michalski <royaltm75@gmail.com>
2022-12-09 07:10:24 +0100

    bigger update

--- master/rust/implementors/core/ops/arith/trait.Sub.js
+++ master/rust/implementors/core/ops/arith/trait.Sub.js
@@ -1,10 +1,3 @@
-(function() {var implementors = {};
-implementors["plasma"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Sub.html\" title=\"trait core::ops::arith::Sub\">Sub</a>&lt;<a class=\"struct\" href=\"plasma/struct.PixelRgb.html\" title=\"struct plasma::PixelRgb\">PixelRgb</a>&gt; for <a class=\"struct\" href=\"plasma/struct.PixelRgb.html\" title=\"struct plasma::PixelRgb\">PixelRgb</a>",synthetic:false,types:["plasma::color::PixelRgb"]},];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        
-})()
+(function() {var implementors = {
+"plasma":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Sub.html\" title=\"trait core::ops::arith::Sub\">Sub</a>&lt;<a class=\"struct\" href=\"plasma/struct.PixelRgb.html\" title=\"struct plasma::PixelRgb\">PixelRgb</a>&gt; for <a class=\"struct\" href=\"plasma/struct.PixelRgb.html\" title=\"struct plasma::PixelRgb\">PixelRgb</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- master/rust/source-files.js
+++ master/rust/source-files.js
@@ -1,3 +1,4 @@
-var N = null;var sourcesIndex = {};
-sourcesIndex["plasma"] = {"name":"","dirs":[{"name":"mixers","dirs":[],"files":["mod.rs","plasma_default.rs","plasma_experimental.rs","plasma_fire.rs"]}],"files":["color.rs","lib.rs","mixer.rs","phase_amp.rs","pixel_buffer.rs","plasma.rs","simd_polyfill.rs"]};
+var sourcesIndex = JSON.parse('{\
+"plasma":["",[["mixers",[],["mod.rs","plasma_default.rs"]]],["color.rs","lib.rs","mixer.rs","phase_amp.rs","pixel_buffer.rs","plasma.rs","simd_polyfill.rs"]]\
+}');
 createSourceSidebar();
